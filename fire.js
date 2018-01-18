886589b069d46358241a891d8a85ed8e2b054676
Kevin <kevinma91@yahoo.com>
2018-01-17 18:14:39 -0800

    initial commit

---
+++ fire.js
@@ -0,0 +1,81 @@
+(function () {
+    var vm = this;
+    var config = {
+      apiKey: "AIzaSyDG2AnvNDb81pot-H0b3OEJt1GJiAOOVH0",
+      authDomain: "crypton-materialkit.firebaseapp.com",
+      databaseURL: "https://crypton-materialkit.firebaseio.com",
+      projectId: "crypton-materialkit",
+    };
+    firebase.initializeApp(config);
+
+    var db = firebase.database();
+
+    document.getElementById("submitEmail").onclick = submit;
+
+    function submit(e) {
+        e.preventDefault();
+
+        var id = Date.now()
+        var email = $('#emailModal #emailInput').val();
+        var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (re.test(email)) {
+            db.ref('emails/' + id).set({
+                id: id,
+                email: email
+            });
+            $('#emailInput, #emailHelp').css("visibility", "hidden");
+            $('#submitEmail').prop("disabled", true);
+            $("#result").text(email + " was added successfully!");
+            // $("#thankYou").text("Thank you for joining Crypton's Beta waitlist.");
+            $("#result").css("color", "palegreen");
+            delayedAlert();
+
+
+            //
+        } else {
+            $('#result').css("color", '#eee8aa');
+            $("#result").text(email + " is not a valid email, please try again. 🤔");
+        }
+    };
+
+    vm.openModal = () => {
+        $('#emailModal').on('shown.bs.modal', function () {
+          $('#emailInput').focus();
+        });
+    }
+
+    vm.hideModal = () => {
+        $('#emailModal').modal('hide');
+        resetModal();
+    }
+
+    function resetModal() {
+        $('#emailModal, #emailInput').val('');
+        $("#result").text('');
+        $("#result").css("color", "none");
+        $('#submitEmail').prop("disabled", false);
+        $('#emailInput, #emailHelp').css("visibility", "visible");
+
+    }
+
+    var timeoutID;
+
+    function delayedAlert() {
+      timeoutID = window.setTimeout(slowAlert, 800);
+
+    }
+
+    function slowAlert() {
+      alert('Thanks for your interest in Crypton.\nYou will receive a confirmation email shortly.');
+      $('#emailModal').modal('hide');
+      var modaltimeoutID;
+      modaltimeoutID = window.setTimeout(resetModal, 500);
+
+    };
+
+
+})()
+
+function openModal () {
+  $('#emailModal').on('shown.bs.modal')
+}
